527039bc10c4e9c3034f7678869f3b8dcc38c599
Stanislav Nikolov <stanislav.ltb@gmail.com>
2016-02-20 10:22:02 +0200

    .

--- 2016_02_13_task2.cpp
+++ 2016_02_13_task2.cpp
@@ -1,1 +1,48 @@
-/home/stanislav/c++/delqn/2016.02.13/2.cpp
+#include <iostream>
+#include <vector>
+
+int n, k;
+
+unsigned long long fact(int n, int m)
+{
+	if(n == m)
+		return m;
+	int a = 1;
+	for(int i = m;i <= n;++ i)
+		a *= i;
+	return a;
+}
+
+int main()
+{
+	std::cin >> n >> k;
+
+	int cycle = 1;
+	std::vector<char> sym;
+	for(int i = 0;i < 26;++ i)
+		sym.push_back((char)(i+'A'));
+
+	while(true)
+	{
+		int first = 0;
+
+		while(k > fact(26-cycle,26-n+1))
+		{
+			k -= fact(26-cycle,26-n+1);
+			first ++;
+		}
+
+		std::cout << sym[first];
+		auto it = sym.begin();
+		for(int i = 0;i < first;++ i)
+			it ++;
+		sym.erase(it);
+
+		cycle ++;
+		if(cycle == n+1)
+		{
+			std::cout << std::endl;
+			return 0;
+		}
+	}
+}
--- 2016_02_20_task1.cpp
+++ 2016_02_20_task1.cpp
@@ -1,1 +1,47 @@
-/home/stanislav/c++/delqn/2016.02.20/1.cpp
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+std::vector<std::pair<int, int>> polygons[102400];
+
+double surface(unsigned idx)
+{
+	double s = 0;
+
+	while(polygons[idx].size() >= 3)
+	{
+		int x1 = polygons[idx][0].first;
+		int y1 = polygons[idx][0].second;
+		int x2 = polygons[idx][1].first;
+		int y2 = polygons[idx][1].second;
+		int x3 = polygons[idx][2].first;
+		int y3 = polygons[idx][2].second;
+		s += abs(0.5*((x1-x2)*(y1+y2)+(x2-x3)*(y2+y3)+(x3-x1)*(y3+y1)));
+		auto it = polygons[idx].begin();
+		it ++;
+		polygons[idx].erase(it);
+	}
+
+	return s;
+}
+
+int main()
+{
+	unsigned n;
+	double result = 0;
+	std::cin >> n;
+	for(unsigned i = 0;i < n;++ i)
+	{
+		unsigned m;
+		std::cin >> m;
+		for(unsigned j = 0;j < m;++ j)
+		{
+			int x, y;
+			std::cin >> x >> y;
+			polygons[i].push_back({x, y});
+		}
+		result += surface(i);
+	}
+	std::cout << result << std::endl;
+	return 0;
+}
